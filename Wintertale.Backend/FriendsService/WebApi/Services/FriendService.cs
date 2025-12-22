83d96b20b6bec74687b6ccd5610ae07738fe3d5b
rotat1ck <rotatick32151@gmail.com>
2025-12-22 06:07:16 +0300

    FriendService Create & Update functions

--- Wintertale.Backend/FriendsService/WebApi/Services/FriendService.cs
+++ Wintertale.Backend/FriendsService/WebApi/Services/FriendService.cs
@@ -4,6 +4,8 @@ using FriendsService.Application.DTOs.Responses;
 using FriendsService.Application.Interfaces.Repositories;
 using FriendsService.Application.Interfaces.Services;
 using FriendsService.Application.Common.Exceptions;
+using FriendsService.Domain.Enums;
+using FriendsService.Domain.Models;
 
 namespace FriendsService.WebApi.Services {
     public class FriendService : IFriendService {
@@ -58,14 +60,57 @@ namespace FriendsService.WebApi.Services {
         }
 
         public async Task<FriendResponse> CreateFriendAsync(CreateFriendRequest request, string requesterId) {
-            var user = await userRepository.GetUserByPhoneAsync(request.phone)
+            var friendUser = await userRepository.GetUserByPhoneAsync(request.phone)
                 ?? throw new UnprocessableException("Пользователь с таким номером телефона не найден");
 
-            var existCheck = await repository.GetFriendByUserAsync();
+            if (friendUser.id.ToString() == requesterId) {
+                throw new InvalidActionException("Невозможно отправить запрос самому себе");
+            }
+
+            var existCheck = await repository.GetFriendByUserAsync(friendUser, requesterId);
+            if (existCheck != null) {
+                if (existCheck.status == (int)FriendStatusEnum.Rejected) {
+                    var updateRequest = mapper.Map<UpdateFriendRequest>(request);
+                    updateRequest.status = (int)FriendStatusEnum.Send;
+
+                    return await UpdateFriendAsync(updateRequest, requesterId);
+                }
+
+                throw new InvalidActionException("Вы уже отправили запрос этому пользователю");
+            }
+
+            var friend = mapper.Map<Friend>(request);
+            friend.user_id_requester = Guid.Parse(requesterId);
+            friend.user_id_receiver = friendUser.id;
+            friend = await repository.CreateFriendAsync(friend);
+
+            return mapper.Map<FriendResponse>(friend);
         }
 
         public async Task<FriendResponse> UpdateFriendAsync(UpdateFriendRequest request, string requesterId) {
+            var friendUser = await userRepository.GetUserByPhoneAsync(request.phone)
+                ?? throw new UnprocessableException("Пользователь с таким номером телефона не найден");
 
+            if (friendUser.id.ToString() == requesterId) {
+                throw new InvalidActionException("Невозможно обновить несуществующий запрос дружбы самому себе");
+            }
+
+            var friend = await repository.GetFriendByUserAsync(friendUser, requesterId)
+                ?? throw new UnprocessableException("Запрос дружбы не найден");
+
+            if (!Enum.IsDefined(typeof(FriendStatusEnum), request.status!)) {
+                throw new NotFoundException("Статус не определен");
+            }
+
+            Guid userId = Guid.Parse(requesterId);
+
+            if (userId == friend.user_id_requester && request.status == (int)FriendStatusEnum.Accepted) {
+                throw new AccessDeniedException("Отправитель не может подтвердить запрос");
+            }
+
+            mapper.Map(request, friend);
+            friend = await repository.UpdateFriendAsync(friend);
+            return mapper.Map<FriendResponse>(friend);
         }
 
         public async Task RemoveFriendAsync(RemoveFriendRequest request, string requesterId) {
